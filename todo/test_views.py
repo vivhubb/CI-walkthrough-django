afc4d318c362ee2318eb2f8431abebc82f8b7710
vivienne <vivienne.2214@yahoo.com>
2022-12-25 08:46:38 +0000

    fix line too long and indentation

--- todo/test_views.py
+++ todo/test_views.py
@@ -42,7 +42,8 @@ class TestViews(TestCase):
 
     def test_can_edit_item(self):
         item = Item.objects.create(name='Test Todo Item', done=True)
-        response = self.client.post(f'/edit/{item.id}', {'name': "Updated Name"})
+        response = self.client.post(f'/edit/{item.id}',
+                                    {'name': "Updated Name"})
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertEqual(updated_item.name, 'Updated Name')
