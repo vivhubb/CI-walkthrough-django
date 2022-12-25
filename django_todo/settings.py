01f6e7ccc0fc79c87123182cac4d335c21427016
vivienne <vivienne.2214@yahoo.com>
2022-12-25 10:17:26 +0000

    fix allowed host - django

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-yt*cq5fgwyae9xb)kfsgmi!o9ir&ub*$y@wt)w0pty1@q+w(1o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-walkthrough.herokuapp.com']
 
 
 # Application definition
